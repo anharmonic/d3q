3583a4e00b62f265daa1ce6314a5bc352556c287
paulatto <paulatz@gmail.com>
2016-04-13 09:14:06 +0000

    Nanosecond-granularity timer should now work on mac os x (please test, I cannot)

--- D3Q/thermal2/nanosec.c
+++ D3Q/thermal2/nanosec.c
@@ -3,19 +3,36 @@
 !  released under the CeCILL licence v 2.1
 !  <http://www.cecill.info/licences/Licence_CeCILL_V2.1-fr.txt>
 */
-#if defined(__APPLE__)
+/*#if defined(__APPLE__)
 double c_nanosec()
 { return 0.;  }
-#else
+#else*/
+
 #include <time.h>
+#include <sys/time.h>
+
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
 
 double c_nanosec()
 {
         double static new_epoch = -1.;
-        struct timespec T;
-        clock_gettime(CLOCK_REALTIME, &T);
-        if ( new_epoch < 0.) new_epoch = (double) T.tv_sec;
-        return (((double)T.tv_sec-new_epoch) + ((double)T.tv_nsec)*1.e-9);
-}
+        struct timespec ts;
+
+#if defined(__MACH__)
+        clock_serv_t cclock;
+        mach_timespec_t mts;
+        host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+        clock_get_time(cclock, &mts);
+        mach_port_deallocate(mach_task_self(), cclock);
+        ts.tv_sec = mts.tv_sec;
+        ts.tv_nsec = mts.tv_nsec;
+#else
+        clock_gettime(CLOCK_REALTIME, &ts);
 #endif
+        if ( new_epoch < 0.) new_epoch = (double) ts.tv_sec;
+        return (((double)ts.tv_sec-new_epoch) + ((double)ts.tv_nsec)*1.e-9);
+}
 
