75aabd0401cd36a647ae22fb95c42273d67beb6f
paulatz <paulatz@gmail.com>
2017-08-30 14:18:18 +0000

    import_phonopy.py : a tool to importe force constant files computed with phonopy and a certain austrian code fc2mat2R.sh : convert the force constant file of standard quantum-esreoss q2r to the optimized format required by thermal2 (by using matdyn.x and d3_q2r.x)

---
+++ D3Q/tools/fc2mat2R.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+input="fc"
+output="mat2R_fc"
+sum_rule="no"
+
+rand=$RANDOM$RANDOM
+header="header-${rand}"
+dyn="dyn-${rand}_"
+q2r="q2r-${rand}"
+tmpdir="tmp"
+
+print_help(){
+        echo "Take a force constant file from q2r (or import_phonopy.py) and"
+        echo "convert it to d3_q2r.x format, doing a back and forward FFT"
+        echo
+	echo "Syntax:"
+	echo "  $0 [-i FC.input] [-o FC.out] [-a ASR_TYPE]"
+        echo
+        echo "FC.in:  input force constants (normal q2r.x format) default: 'fc'"
+        echo "FC.out: output force constants (optimized d3_q2r.x format) default: 'mat2R'"
+        echo "ASR_TYPE: sum rule method to apply default: no"
+        exit $1
+}
+
+while getopts "h?a:i:o:" opt; do
+    case "$opt" in
+    h|\?)
+        print_help 0
+        ;;
+    a)  sum_rule=$OPTARG
+        ;;
+    i)  input=$OPTARG
+        ;;
+    o)  output=$OPTARG
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+mkdir -p ${tmpdir}
+
+
+nqline=$(grep -E "^ *[1-9]+[0-9]* +[1-9]+[0-9]* +[1-9]+[0-9]* *$" ${input}|head -n 1)
+
+n1=$(echo $nqline|awk '{print $1}')
+n2=$(echo $nqline|awk '{print $2}')
+n3=$(echo $nqline|awk '{print $3}')
+
+nn=$[n1*n2*n3]
+echo "Number of files: $nn, grid:  $n1, $n2, $n3"
+
+function n2q {
+  q1=$(echo $1./$4.|/usr/bin/bc -l)
+  q2=$(echo $2./$5.|/usr/bin/bc -l)
+  q3=$(echo $3./$6.|/usr/bin/bc -l)
+  echo $q1 $q2 $q3
+}
+
+#prepare ${header} file
+grep -B100 "^ *${n1}  *${n2}  *${n3} *$" ${input} > ${tmpdir}/${header}-0
+nl=0
+
+echo "Artificial dyn mat file " > ${tmpdir}/${header}
+echo " " >> ${tmpdir}/${header}
+  head -n1 "${tmpdir}/${header}-0" >> ${tmpdir}/${header}
+# add a damn line between celldm and lattice vectors if ibrav=0
+if grep -qE "^ +[1-9]+ +[1-9]+ +0 +([0-9.]+ *){6,6} *$" ${tmpdir}/${header}-0; then
+  echo "lattice vectors " >> ${tmpdir}/${header} 
+fi
+  nl=$(wc -l ${tmpdir}/${header}-0|awk '{print $1}')
+  tail -n $[$nl-1] ${tmpdir}/${header}-0 |head -n $[$nl-3]>> ${tmpdir}/${header}
+
+
+cat << eof > ${tmpdir}/${q2r}
+ &input
+   fildyn  =  ''
+   flfrc   =  'mat2R'
+ /
+$n1 $n2 $n3
+$nn
+eof
+
+# generate, one by one, the dynamical matrices files
+# for the list of q-points in the dyn0 file
+n=0
+for i1 in $(seq 0 $[n1-1]); do
+for i2 in $(seq 0 $[n2-1]); do
+for i3 in $(seq 0 $[n3-1]); do
+  let n++
+  q=$(n2q $i1 $i2 $i3 $n1 $n2 $n3)
+  echo $q
+  echo ${tmpdir}/${dyn}${n} >> ${tmpdir}/${q2r}
+
+cat << eof | matdyn.x > ${tmpdir}/out-${rand}.${n}
+&input
+  flfrc='${input}'
+  asr='${sum_rule}' 
+  fldyn='${tmpdir}/${dyn}${n}'
+  flfrq='${tmpdir}/freq-${rand}'
+  flvec='${tmpdir}/modes-${rand}'
+  q_in_cryst_coord = .true.
+/
+1
+${q}
+eof
+
+cat ${tmpdir}/${header}  >  ${tmpdir}/ref_${dyn}${n}
+cat ${tmpdir}/${dyn}${n} >> ${tmpdir}/ref_${dyn}${n}
+mv ${tmpdir}/ref_${dyn}${n}  ${tmpdir}/${dyn}${n}
+
+done
+done
+done
+
+d3_q2r.x < ${tmpdir}/${q2r} > ${tmpdir}/out-${rand}.d3_q2r
+
+rm -f ${tmpdir}/*${rand}*
+ls ${tmpdir}/* >/dev/null 2>&1 || rm -r ${tmpdir}
+
---
+++ D3Q/tools/import_phonopy.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+
+print """
+
+This is a very rough script that imports the FORCE_CONSTANT
+file produced by phonopy (by A. Togo). It assumes that the
+file has been produced by a VASP calculation, it will try to
+read three files:
+1. POSCAR with the unit cell
+2. SPOSCAR with the super-cell (without any displacement)
+3. FORCE_CONSTANTS with the force costants
+
+This code uses the ASE python library, you can usually install
+it with:
+ pip install ase
+or by hand (https://wiki.fysik.dtu.dk/ase/).
+
+I do not have time to add support for any other code,
+you should be able to do it yourself. Please not that the
+units of measur used inside FORCE_CONSTANTS seems to depend
+on the code used to compute them.
+
+The resulting FC file will be saved as 'fc'.
+
+NOTE THAT THE fc FILE IS NOT SUITABLE FOR USE IN THERMAL2
+BECAUSE IT IS NOT OPTIMIZED FOR FOURIER INTERPOLATION
+YOU WILL HAVE TO CONVERT IT TO THERMAL2 USING THE SCRIPT
+fc2mat2R.sh FROM THE TOOLS DIRECTORY!
+
+
+"""
+
+MASS_DALTON_TO_RY = 0.5*1822.88839
+
+HARTREE_SI        = 4.35974394E-18
+ELECTRONVOLT_SI   = 1.602176487E-19
+AUTOEV           = HARTREE_SI / ELECTRONVOLT_SI
+RYTOEV           = AUTOEV / 2.
+AUTOEV            = HARTREE_SI / ELECTRONVOLT_SI
+
+BOHR_RADIUS_ANGS  = .52917721067
+ANGSTROM_AU       = 1./BOHR_RADIUS_ANGS
+
+# Force constants seem to be already in Ry/bohr^2, at least when
+# produced from QE calculation
+F_FACTOR = 1./RYTOEV/ANGSTROM_AU**2
+
+print "Force constant unit conversion factor:", F_FACTOR
+
+def atoms2species(listat):
+  nat = len(listat)
+  idxat = [0]*nat
+  listsp = []
+  invsp = []
+  #
+  for i in range(nat):
+    try:
+      idxat[i] = listsp.index(listat[i])
+    except ValueError:
+      listsp.append(listat[i])
+      idxat[i] = len(listsp)-1
+      invsp.append(i)
+  return listsp, invsp, idxat
+
+def supercell2cell(nat, atuc, natsc, atsc, cell, rcell, nq1,nq2,nq3):
+  from numpy import dot, transpose, zeros, int
+  listrbig = zeros(shape=(natsc,3),dtype=int)
+  idxsc = [0]*natsc
+  trcell = transpose(rcell)
+  for i in range(natsc):
+    for j in range(nat):
+      rbig = atsc[i,:] - atuc[j,:]
+      rbigx =  dot(trcell, rbig)
+      rbigi = [round(rbigx[0]), round(rbigx[1]), round(rbigx[2])]
+      delta = norm(rbigx-rbigi)
+      if delta < 1.e-6:
+        listrbig[i][0] = int(rbigi[0]) % nq1
+        listrbig[i][1] = int(rbigi[1]) % nq2
+        listrbig[i][2] = int(rbigi[2]) % nq3
+        idxsc[i] = j
+        break
+  return idxsc, listrbig
+
+from sys import stderr
+import ase.io.vasp
+fin = open("POSCAR")
+aseuc = ase.io.vasp.read_vasp(fin)
+fin.close()
+
+nat  = len(aseuc)
+csym = aseuc.get_chemical_symbols()
+cmass= MASS_DALTON_TO_RY * aseuc.get_masses()
+
+listsp, invsp, idxat = atoms2species(csym)
+
+nsp = len(invsp)
+
+
+cell = aseuc.get_cell()
+from  numpy.linalg import norm
+alat = norm(cell[0,:])
+rcell = aseuc.get_reciprocal_cell()
+
+fout = open("fc", 'w')
+
+fout.write( """{:6} {:6} {:6} {:15.10f} {:15.10f} {:15.10f} {:15.10f} {:15.10f} {:15.10f} \n""".format(nsp, nat, 0, alat/0.52917721067 , 0.,0.,0.,0.,0.) )
+fout.write( """{:15.10f} {:15.10f} {:15.10f}\n""".format(cell[0,0]/alat, cell[0,1]/alat, cell[0,2]/alat) )
+fout.write( """{:15.10f} {:15.10f} {:15.10f}\n""".format(cell[1,0]/alat, cell[1,1]/alat, cell[1,2]/alat) )
+fout.write( """{:15.10f} {:15.10f} {:15.10f}\n""".format(cell[2,0]/alat, cell[2,1]/alat, cell[2,2]/alat) )
+
+for i in range(nsp):
+  fout.write( """       {:6} '{:4}' {:15.10f} \n""".format( i+1, csym[invsp[i]], cmass[invsp[i]] ) )
+
+cpos = aseuc.get_positions()
+for i in range(nat):
+  fout.write( """ {:6} {:6} {:15.10f} {:15.10f} {:15.10f}\n""".format( i+1, idxat[i]+1, cpos[i,0]/alat , cpos[i,1]/alat, cpos[i,2]/alat) )
+
+# no effective charges:
+fout.write("F\n")
+
+fin = open("SPOSCAR")
+asesc = ase.io.vasp.read_vasp(fin)
+fin.close()
+
+scell = asesc.get_cell()
+nq1 =  int(round(norm(scell[0,:])/norm(cell[0,:])))
+nq2 =  int(round(norm(scell[1,:])/norm(cell[1,:])))
+nq3 =  int(round(norm(scell[2,:])/norm(cell[2,:])))
+ncells = nq1*nq2*nq3
+print "Supercell size: ", nq1, nq2, nq3
+
+fout.write("{:6} {:6} {:6} \n".format(nq1, nq2, nq3))
+
+spos = asesc.get_positions()
+
+natsc = len(asesc)
+
+idxsc, idxcell = supercell2cell(nat, cpos, natsc, spos, cell, rcell, nq1,nq2,nq3)
+
+from numpy import zeros
+fc = zeros(shape=(3,3,nat,nat,nq1,nq2,nq3))
+
+
+fin = open("FORCE_CONSTANTS")
+natsc_ = int(fin.readline())
+if natsc_ != natsc :
+  stderr.write( "Wrong number of atoms in FC file" )
+  raise Exception 
+
+nread=0
+nskip=0
+for i in range(natsc):
+  idat_i = idxsc[i]
+  ci1,ci2,ci3 = idxcell[i]
+  for j in range(natsc):
+    i_,j_ = [int(x) for x in fin.readline().split()]
+    if [i_-1,j_-1] != [i,j]:
+      stderr.write( i,j,i_-1,j_-1 )
+      stderr.write( "Unexpected atom" )
+      raise Exception 
+
+    fxx, fxy, fxz = [float(x) for x in fin.readline().split()]
+    fyx, fyy, fyz = [float(x) for x in fin.readline().split()]
+    fzx, fzy, fzz = [float(x) for x in fin.readline().split()]
+    if ci1 == 0 and ci2 == 0 and ci3 == 0:
+      nread += 1
+      idat_j = idxsc[j]
+      c1,c2,c3 = idxcell[j]
+      fc[0][0][idat_i][idat_j][c1][c2][c3] = fxx
+      fc[1][0][idat_i][idat_j][c1][c2][c3] = fyx
+      fc[2][0][idat_i][idat_j][c1][c2][c3] = fzx
+      fc[0][1][idat_i][idat_j][c1][c2][c3] = fxy
+      fc[1][1][idat_i][idat_j][c1][c2][c3] = fyy
+      fc[2][1][idat_i][idat_j][c1][c2][c3] = fzy
+      fc[0][2][idat_i][idat_j][c1][c2][c3] = fxz
+      fc[1][2][idat_i][idat_j][c1][c2][c3] = fyz
+      fc[2][2][idat_i][idat_j][c1][c2][c3] = fzz
+    else:
+      nskip += 1
+fin.close()
+
+if (nskip+nread)/nread != natsc/nat:
+  stderr.write( "Skipped the wrong amount {} {}".format( nskip, nread) )
+  raise Exception
+
+for alpha in range(3):
+  for beta in range(3):
+    for i in range(nat):
+      for j in range(nat):
+        fout.write( "{:4}{:4}{:4}{:4}\n".format(alpha+1, beta+1, i+1, j+1) )
+        for iq3 in range(nq3):
+          for iq2 in range(nq2):
+            for iq1 in range(nq1):
+              fout.write( "{:4}{:4}{:4}{:20.10e}\n".format(iq1+1, iq2+1, iq3+1, fc[alpha][beta][i][j][iq1][iq2][iq3]*F_FACTOR) )
+
+fout.close()
+print "Force constants written to file 'fc'"
+
+
+
+
+
