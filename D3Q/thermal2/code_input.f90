fb7956bcaaefc89521cf3a3dc1c05abd32f1ded4
paulatz <paulatz@gmail.com>
2018-03-06 14:50:14 +0000

    bugfix

--- D3Q/thermal2/code_input.f90
+++ D3Q/thermal2/code_input.f90
@@ -495,14 +495,17 @@ MODULE code_input
           IF(ionode) READ(input_unit,*,iostat=ios) e1
           CALL mpi_broadcast(ios)
           IF(ios/=0) CALL errore("READ_INPUT", "plane 1", 1)
+          CALL mpi_broadcast(3, e1)
           
           IF(ionode) READ(input_unit,*,iostat=ios) e2
           CALL mpi_broadcast(ios)
           IF(ios/=0) CALL errore("READ_INPUT", "plane 1", 1)
+          CALL mpi_broadcast(3, e2)
 
           IF(ionode) READ(input_unit,*,iostat=ios) xq0
           CALL mpi_broadcast(ios)
           IF(ios/=0) CALL errore("READ_INPUT", "plane 1", 1)
+          CALL mpi_broadcast(3, xq0)
           
           line=''
           !this is done later
