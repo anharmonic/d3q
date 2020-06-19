3f284b8575cb08d0de034feafa27d44df4f7c072
paulatz <paulatz@gmail.com>
2020-06-19 10:22:09 +0000

    wip

--- D3Q/thermal2/PROGRAM_make_wedge.f90
+++ D3Q/thermal2/PROGRAM_make_wedge.f90
@@ -50,7 +50,8 @@ PROGRAM make_wedge
   USE control_lr,         ONLY : lgamma
   USE decompose_d2
   USE cmdline_param_module
-  USE input_fc,           ONLY : forceconst2_grid, ph_system_info, read_system, aux_system, read_fc2, div_mass_fc2
+  USE input_fc,           ONLY : forceconst2_grid, ph_system_info, read_system, aux_system, read_fc2, &
+                                 div_mass_fc2, multiply_mass_dyn
   USE fc2_interpolate,    ONLY : fftinterp_mat2, mat2_diag, dyn_cart2pat
   USE asr2_module,        ONLY : impose_asr2
 
@@ -62,13 +63,13 @@ PROGRAM make_wedge
   CHARACTER(len=256) :: fildyn, filout
   INTEGER :: ierr, nargs
   !
-  INTEGER       :: nq1, nq2, nq3, nqmax, nqs, isq (48), imq, nqq
+  INTEGER       :: nq1, nq2, nq3, nqmax, nqs, isq (48), imq, nqq,nq_out
   REAL(DP),ALLOCATABLE      :: x_q(:,:), w_q(:)
-  REAL(DP) :: xq(3), sxq(3,49)
+  REAL(DP) :: xq(3), sxq(3,48)
   !
   LOGICAL :: sym(48), lrigid, skip_equivalence, time_reversal
   !
-  COMPLEX(DP),ALLOCATABLE :: phi(:,:,:,:), d2(:,:), basis(:,:,:)
+  COMPLEX(DP),ALLOCATABLE :: phi(:,:,:,:), d2(:,:), basis(:,:,:), star_dyn(:,:,:)
   REAL(DP),ALLOCATABLE :: decomposition(:)
   INTEGER :: i,j, icar,jcar, na,nb, rank, iq
   TYPE(ph_system_info) :: Sinfo
@@ -126,7 +127,7 @@ PROGRAM make_wedge
   !
   ! Find the reduced grid of q-points:
   skip_equivalence = .FALSE.
-  time_reversal    = .true.
+  time_reversal    = .TRUE.
   nq1 = 8
   nq2 = 8
   nq3 = 8
@@ -167,20 +168,38 @@ PROGRAM make_wedge
     ! finally this does some of the above again and also computes rtau...
     CALL sgam_lr(at, bg, nsym, s, irt, tau, rtau, nat)
     !
-    ! the next subroutine uses symmetry from global variables
+    ! the next subroutine uses symmetry from global variables to find he basis of crystal-symmetric
+    ! matrices at this q point
     CALL find_d2_symm_base(xq, rank, basis)
     !
-!    DO i = 1, rank
-!      d2 = basis(:,:,i)
-!      WRITE(100*iq+i,*) i, xq
-!      CALL star_q(xq, at, bg, nsym, s, invs, nqs, sxq, isq, imq, .true. )
-!      ! Generate the star of q and write it on file (i actually want to have it in a variable!)
-!      CALL q2qstar_ph (d2, at, bg, nat, nsym, s, invs, irt, rtau, &
-!                       nqs, sxq, isq, imq, 100*iq+i)
-!    ENDDO
-  
+    ! Calculate the list of points making up the star of q
+    CALL star_q(xq, at, bg, nsym, s, invs, nqs, sxq, isq, imq, .true. )
+    IF(imq/=0) THEN
+       nq_out = nqs
+       ALLOCATE(star_dyn(3*nat,3*nat, nqs))
+    ELSE ! In this cas -q is not in the star of q, but I can recover it by time-reversal D(-q) = D(q)^*
+       nq_out = 2*nqs
+       IF(nq_out>48) CALL errore("make_wedge","unexpected imq=0 and nqs>48/2",1)
+       ALLOCATE(star_dyn(3*nat,3*nat, 2*nqs))
+       sxq(:,nqs+1:2*nqs) = -sxq(:,1:nqs)
+    ENDIF
+
     ! Interpolate the system dynamical matrix at this q
     CALL fftinterp_mat2(xq, Sinfo, fc, d2)
+    ! Remove the mass factor, I cannot remove it before because al the effective
+    ! charges code assumes it is there
+    d2 = multiply_mass_dyn(Sinfo,d2)
+    !
+    star_dyn = 6666._dp
+!      ! Rotate the dynamical matrices making up the star of q it on file (i actually want to have it in a variable!)
+    CALL make_qstar_d2 (d2, at, bg, nat, nsym, s, invs, irt, rtau, &
+                         nqs, sxq, isq, imq, nq_out, star_dyn)
+    DO i = 1, nq_out
+       WRITE(stdout,'(i4,3f12.4,l2, 1f15.9)') i, sxq(:,i), (i>nqs), dotprodmat(3*nat,star_dyn(:,:,i), star_dyn(:,:,i))
+    ENDDO
+    DEALLOCATE(star_dyn)
+!    ENDDO
+  
     !CALL compact_dyn(nat, d2, phi)
     !  print*, d2
     print*, "== DECOMPOSITION =="
