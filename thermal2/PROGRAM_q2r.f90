bad19c66c667509c46f459befa6caefebe033f2d
Lorenzo Paulatto <paulatz@gmail.com>
2026-06-08 09:13:59 +0200

    Removed double warning

--- thermal2/PROGRAM_q2r.f90
+++ thermal2/PROGRAM_q2r.f90
@@ -156,10 +156,6 @@ PROGRAM q2r
      IF(nopbc(2).and.(nr(2)>1) ) CALL errore('q2r','required non-periodic along direction 2, but more than one q-point',2)
      IF(nopbc(3).and.(nr(3)>1) ) CALL errore('q2r','required non-periodic along direction 3, but more than one q-point',3)     !
      !
-     IF(.not. nopbc(1).and.(nr(1)==1) ) WRITE(*,*) 'WARNING: required periodic along direction 1, but only one q-point'
-     IF(.not. nopbc(2).and.(nr(2)==1) ) WRITE(*,*) 'WARNING: required periodic along direction 2, but only one q-point'
-     IF(.not. nopbc(3).and.(nr(3)==1) ) WRITE(*,*) 'WARNING: required periodic along direction 3, but only one q-point'
-     !
      ! D matrix (analytical part)
      !
      ntyp = ntypx ! avoids spurious out-of-bound errors
